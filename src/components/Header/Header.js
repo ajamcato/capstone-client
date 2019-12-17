2cfc58c52ad63d39a07d116a29be4f1ff889f07e
ajamcato <ajamcato87@gmail.com>
2019-12-17 14:36:36 -0500

    Changed header to Event Planner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,8 @@ import Navbar from 'react-bootstrap/Navbar'
 const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
+    <Nav.Link href="#create-event">Create Event</Nav.Link>
+    <Nav.Link href="#events">Events</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
 )
@@ -25,7 +27,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      Event Planner
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
